829a423839e13db9f0ef9aec489d197cb9a526d3
SadVitorGomez <7vitorgomez@gmail.com>
2021-06-24 00:46:13 -0300

    Day 2: Firebase authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,23 @@
 import { useHistory } from 'react-router-dom'
 
+import { Button } from '../components/Button'
+import { useAuth } from '../hooks/useAuth'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
 import '../styles/auth.scss'
-import { Button } from '../components/Button'
 
 export function Home() {
   const history = useHistory()
+  const { user, signInWithGoogle } = useAuth()
 
-  function navigateToNewRoom() {
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
+
     history.push('/rooms/new')
   }
 
@@ -27,7 +34,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room" onClick={navigateToNewRoom}>
+          <button className="create-room" onClick={handleCreateRoom}>
             <img src={googleIconImg} alt="Logo do Google" />
             Crie a sua sala com o Google
           </button>
